d67dc1e1a1176adc272430d60e2197b35f49a399
Juan Castillo <castillojuan1000@gmail.com>
2019-09-11 00:10:36 -0400

    title, box-shadow, easeout

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,11 +7,20 @@ const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 
-let showMenu = false; 
+//TODO: Pages
+const contact = document.getElementById('contact');
+const home = document.getElementById('home');
+const work = document.getElementById('work');
+
+
+
+
+
+let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
-function toggleMenu(){
-    if(!showMenu){
+function toggleMenu() {
+    if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
@@ -19,7 +28,7 @@ function toggleMenu(){
         navItems.forEach(item => item.classList.add('show'));
         // set Menu State
         showMenu = true;
-    } else{
+    } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
@@ -32,3 +41,60 @@ function toggleMenu(){
 
     }
 }
+
+menu.addEventListener('click', e => {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        // set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => {
+            item.classList.remove('show')
+        });
+        // set Menu State
+        showMenu = false;
+
+    }
+
+
+})
+
+
+
+// work.addEventListener('click', e => {
+//     if (!showMenu) {
+//         menuBtn.classList.add('close');
+//         menu.classList.add('show');
+//         menuNav.classList.add('show');
+//         menuBranding.classList.add('show');
+//         navItems.forEach(item => item.classList.add('show'));
+//         // set Menu State
+//         showMenu = true;
+//     } else {
+//         menuBtn.classList.remove('close');
+//         menu.classList.remove('show');
+//         menuNav.classList.remove('show');
+//         menuBranding.classList.remove('show');
+//         navItems.forEach(item => {
+//             item.classList.remove('show')
+//         });
+//         // set Menu State
+//         showMenu = false;
+
+//     }
+
+// })
+
+
+
+
+
+
